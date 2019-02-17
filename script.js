fe044895eeff6da5e1707f094bf7a30ff0996fab
alexandr <grandmasteralexandr@gmail.com>
2019-02-17 21:56:57 +0200

    add js file

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const LOGIN_FORM = document.querySelector(".login-form");
+const INFO_FORM = document.querySelector(".info-form");
 const EMAIL = document.getElementById("email");
 const PASS = document.getElementById("pass");
 const USERNAME = document.getElementById("username");
@@ -6,3 +8,9 @@ const REFERENCES = document.getElementById("preferences");
 const LOGIN_BUTTON = document.getElementById("login-form-button");
 const INFO_BUTTON = document.getElementById("info-form-button");
 
+LOGIN_BUTTON.addEventListener("click", login);
+
+function login() {
+    LOGIN_FORM.classList.add("hidden");
+    INFO_FORM.classList.remove("hidden");
+}
