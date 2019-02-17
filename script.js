0533a2b32c85d6ca62a0a17f08c51b8b306cdb06
alexandr <grandmasteralexandr@gmail.com>
2019-02-17 21:33:58 +0200

    add js file

---
+++ script.js
@@ -0,0 +1,8 @@
+const EMAIL = document.getElementById("email");
+const PASS = document.getElementById("pass");
+const USERNAME = document.getElementById("username");
+const GREAT_HOUSE = document.getElementById("great-house");
+const REFERENCES = document.getElementById("preferences");
+const LOGIN_BUTTON = document.getElementById("login-form-button");
+const INFO_BUTTON = document.getElementById("info-form-button");
+
