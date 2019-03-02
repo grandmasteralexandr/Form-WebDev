b9ad8aa1505b8edaf0b054b064c8d9aa453dcd8a
alexandr <grandmasteralexandr@gmail.com>
2019-03-02 18:20:33 +0200

    ps3 test slider

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,14 @@
 $(document).ready(() => {
     // $(".slider").slick();
+    $(".slider-container").ikSlider(
+        {
+            responsive: false,
+            pauseOnHover: false,
+            delay: 3000,
+            touch   : false,
+            controls: false,
+            arrows  : false,
+            infinite: true,
+        }
+    );
 });
