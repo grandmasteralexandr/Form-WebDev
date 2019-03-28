417810c15eeb76667f68de91bcb323b8d31a18ab
alexandr <grandmasteralexandr@gmail.com>
2019-03-28 22:13:52 +0200

    ps4 add config

---
+++ app/config.php
@@ -0,0 +1,12 @@
+<?php
+const GREAT_HOUSES_LIST = [
+    "Stark" => "Stark of Winterfell",
+    "Arryn" => "Arryn of the Eyrie",
+    "Baratheon" => "Baratheon of Storm's End",
+    "Greyjoy" => "Greyjoy of Pyke",
+    "Lannister" => "Lannister of Casterly Rock",
+    "Martell" => "Martell of Sunspear",
+    "Targaryen" => "Targaryen of King's Landing",
+    "Tully" => "Tully of Riverrun",
+    "Tyrell" => "Tyrell of Highgarden",
+];
